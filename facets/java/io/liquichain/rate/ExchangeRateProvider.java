3dca86a29307a87d9178c88c09d50c5d1fd2ad94
meveo.admin <no-mail@meveo.com>
2022-04-13 03:53:58 +0000

    Add the script File : io.liquichain.rate.ExchangeRateProviderin the module : mv-liquichain-exchange-rates

---
+++ facets/java/io/liquichain/rate/ExchangeRateProvider.java
@@ -0,0 +1,51 @@
+package io.liquichain.rate;
+
+import java.util.Map;
+import java.time.Instant;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+import io.liquichain.api.payment.ConversionRateScript;
+
+
+public class ExchangeRateProvider extends Script {
+		
+    private String fromCurrency;
+    private String toCurrency;
+    private Instant fromDate;
+    private Instant toDate;
+    private String result;
+
+    public void setFromCurrency(String fromCurrency) {
+        this.fromCurrency=fromCurrency;
+    }
+  
+    public void setToCurrency(String toCurrency) {
+        this.toCurrency=toCurrency;
+    }
+  
+    public void setFromDate(Instant fromDate) {
+        this.fromDate=fromDate;
+    }
+  
+    public void setToDate(Instant toDate) {
+        this.toDate=toDate;
+    }
+  
+  
+    public String getResult() {
+        return result;
+    }
+  
+  
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		result="{"
+          +"from=\""+fromDate+"\",\n"
+          +"to=\""+toDate+"\",\n"
+          +"data=[\n"
+          +"{\"timestamp\":System.currentTimeMillis(),\"value\":"+ConversionRateScript.LCN_TO_EUR+"}\n"
+          +"]\n"
+          +"}";
+	}
+}
